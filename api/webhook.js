19c678b42cafbe7b1e902869ba42ec6306607ead
Marcel2201020012 <marcelaja314@gmail.com>
2025-08-20 21:47:23 +0700

    testing webhook postman

--- api/webhook.js
+++ api/webhook.js
@@ -1,5 +1,17 @@
-import { doc, updateDoc } from 'firebase/firestore';
-import { db } from '../firebase/firebaseConfig';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
@@ -27,6 +39,8 @@ export default async function handler(req, res) {
         console.log("Parsed body:", body);
 
         const { order_id, transaction_status } = body;
+
+        console.log("Looking for order id:", order_id);
 
         if (transaction_status === 'expire') {
             const orderRef = doc(db, 'transaction_id', order_id);
