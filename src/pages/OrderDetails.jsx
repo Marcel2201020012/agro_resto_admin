0f9fac55c5395d86bf0051f827761a27d6d2690b
Marcel2201020012 <marcelaja314@gmail.com>
2025-08-21 12:11:40 +0700

    fix promtion price on order detail

--- src/pages/OrderDetails.jsx
+++ src/pages/OrderDetails.jsx
@@ -175,9 +175,16 @@ export const OrderDetails = () => {
                                 <span>{item.jumlah}x</span>{" "}
                                 <span>{item.name}</span>
                             </div>
-                            <div className="text-right font-semibold">
-                                {new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0 }).format(item.price)}
-                            </div>
+
+                            {item.promotion > 0 ?
+                                <div className="text-right font-semibold">
+                                    {new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0 }).format(item.promotion)}
+                                </div>
+                                :
+                                <div className="text-right font-semibold">
+                                    {new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0 }).format(item.price)}
+                                </div>
+                            }
                         </div>
                     ))}
                 </div>
