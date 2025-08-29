29b339af97af35c8676b22f6cd9e018c48b028a6
Marcel2201020012 <marcelaja314@gmail.com>
2025-08-29 23:54:29 +0700

    update ux

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="w-full bg-agro-color text-white py-2 overflow-hidden fixed bottom-0 left-0">
       <div className="whitespace-nowrap animate-marquee">
-        Copyright (c) 2025 Hotel Agro Bintan. All rights reserved. This website is developed exclusively for Agro Hotel Bintan. Unauthorized copying, distribution, or modification is prohibited.
+        Copyright (c) 2025 Agro Hotel Bintan. All rights reserved. This website is developed exclusively for Agro Hotel Bintan. Unauthorized copying, distribution, or modification is prohibited.
       </div>
     </footer>
   );
